51dc1cc9535a483dd8c5ce3cb31f2e99d47ffeef
xieshuai <xieshuai@szcomtop.com>
2017-12-22 12:27:39 +0800

    configure

--- config/index.js
+++ config/index.js
@@ -6,12 +6,26 @@
 const path = require('path')
 
 module.exports = {
+  dev: {
+    env: require('./dev.env'),
+    port: process.env.PORT || 8088,
+    autoOpenBrowser: false,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
+  },
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -24,19 +38,5 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-  },
-  dev: {
-    env: require('./dev.env'),
-    port: process.env.PORT || 8080,
-    autoOpenBrowser: true,
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false
   }
 }
---
+++ src/service/axios.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+const api = {
+  /*
+   * GET /api/user/get_info 获取用户信息
+   */
+  getUserInfo: '/job/api/user/get_info',
+}
+
+axios.defaults.timeout = 60000
+
+axios.interceptors.response.use(response => {
+  // 会话过期，请重新登录
+  if (response.data.state === 301) {
+  }
+  return response
+}, err => {
+  return err
+})
+
+export default {
+  get(name, data) {
+    return axios.get(api[name], {
+      params: data || {}
+    })
+  },
+  post(name, data, options) {
+    return axios.post(api[name], data, options)
+  }
+}
